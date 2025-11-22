60ecea9c70d9c13814b7b71a1f7f31755ece9891
abdullah222005 <g.rabbi2005.555@gmail.com>
2025-11-22 11:35:20 +0600

    added personal book collection

--- index.js
+++ index.js
@@ -29,18 +29,51 @@ async function run() {
     await client.connect();
     const db = client.db('the-book-heaven_DB');
     const booksCollection = db.collection('all-books');
+    const usersCollection = db.collection('users');
+    const personalBookCollection = db.collection('myBooks')
 
+      // Users API
+    app.post("/users", async (req, res) => {
+      const newUser = req.body;
+      const email = req.body.email;
+      const query = { email: email };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        res.send({ message: "user already exists." });
+      } else {
+        const result = await usersCollection.insertOne(newUser);
+        res.send(result);
+      }
+    });
+
+    app.get('/users', async(req, res)=>{
+      const cursor = usersCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    // All Books API
     app.get('/all-books', async (req, res)=>{
         const cursor = booksCollection.find();
         const result = await cursor.toArray();
         res.send(result);
     })
 
+    // Book Details API
     app.get('/book-details/:id', async(req, res)=>{
         const id = req.params.id;
         const query = {_id: new ObjectId(id)}
         const result = await booksCollection.findOne(query);
         res.send(result);
+    })
+
+    //Personal Book Collection API
+    app.get('/myBooks', async(req, res)=>{
+      const email = req.query.email;
+      const personalBooks = await personalBookCollection.find({ userEmail: email }).toArray();
+      const mainCollection = await booksCollection.find({ userEmail: email }).toArray();
+      const allBooks = [...personalBooks, ...mainCollection]
+      res.send(allBooks);
     })
 
 
